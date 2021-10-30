31bf652a5e13b14fc9abc00147a3ac4ec169cbe9
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2021-10-30 14:56:40 +0300

    Updates

--- src/functionality.js
+++
@@ -1,6 +0,0 @@
-export const disactivePreviousButton = () => {
-  if (document.getElementsByClassName('isActive')[0]) {
-    const activeButton = document.getElementsByClassName('isActive')[0];
-    activeButton.className = activeButton.className.slice(0, -' isActive'.length);
-  }
-}
